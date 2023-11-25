4ee5dcf83b1ac60b8e0a2b5369e446038c51afc9
ARJUN SINGH NEGI <78934428+ARJU7500@users.noreply.github.com>
2023-11-25 10:01:06 +0000

    update

---
+++ basicjs/basic_string_number.js
@@ -0,0 +1,27 @@
+let score= undefined
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInnumber=Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInnumber)
+
+//"33" => 33
+//"33abc"=> NaN
+// true => 1
+// false=> 0
+
+let isLoggedIn="arjun"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true
+//0=> false
+// ""=> false
+//"arjun"=> true
+
+//string to number
+let someNumber=33
+let stringtoNumber=String(someNumber)
+console.log(stringtoNumber)
+console.log( typeof stringtoNumber)
