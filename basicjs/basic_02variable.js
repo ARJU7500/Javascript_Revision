e73a07b245bed7aca0be1b326af47cee06ce75d8
ARJUN SINGH NEGI <78934428+ARJU7500@users.noreply.github.com>
2023-11-25 09:39:26 +0000

    new update

---
+++ basicjs/basic_02variable.js
@@ -0,0 +1,10 @@
+const accountId=1000987
+var accountGmail= "ram@108"
+let accountName="arjun negi"
+accountCity="jaipur"
+
+// accountId=1009789 not allowed 
+accountGmail="shiv@108"
+accountName="himanshu"
+accountCity="ranikhet"
+console.table([accountId, accountGmail, accountName, accountCity])
---
+++ basicjs/basic_datatype.js
@@ -0,0 +1,4 @@
+let name="Arjun Negi"
+let age= 26
+let num=7668213002
+console.log([name, age, num])
