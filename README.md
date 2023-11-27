f7205b28b2d5c1beb7f25c6ddde7074594d90292
ARJUN SINGH NEGI <78934428+ARJU7500@users.noreply.github.com>
2023-11-27 12:16:31 +0000

    27 nov 23

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Javascript_Revision
-revision part of javascript basic to advance 
+# Javascript_Revision with "chai or code" hitesh sir
+revision part of javascript basic to advance
+day 1st:
+diffrent b/w .text file and .js file.
+multiple variable print together(console.table([var1,var2,........,varn])) used this method we have a table in o/p where we see our value.
+setting write code online (codespace)
+let,const or var ki story.
+data type of js.
+day 2nd:
+1. string:
+a. basic operation performed (+,-,*,/,%)
+b. used when we find to power of any numbers (2**3=8)
+c.  add string
+str1="arjun singh"
+str2= " negi"
+str3=str1+str2;//arjun singh negi
+d. agr first string value h to all value convert in string 
+but if last value is string so first operation perform then convert in string 
+e.  ++ and --;
+
+comprasion in js:
+basic <,>,==.
+compresion when our data type are not same
+("2">1)
+
+//strict check ===
+console.log("2"==2) because of conversion
+console.log("2"===2) because check data types 
--- basicjs/basic_string_number.js
+++ basicjs/basic_string_number.js
@@ -1,27 +1,79 @@
-let score= undefined
-console.log(typeof score)
-console.log(typeof(score))
+// // // // // // let score= undefined
+// // // // // // console.log(typeof score)
+// // // // // // console.log(typeof(score))
 
-let valueInnumber=Number(score)
-console.log(typeof valueInnumber);
-console.log(valueInnumber)
+// // // // // // let valueInnumber=Number(score)
+// // // // // // console.log(typeof valueInnumber);
+// // // // // // console.log(valueInnumber)
 
-//"33" => 33
-//"33abc"=> NaN
-// true => 1
-// false=> 0
+// // // // // // //"33" => 33
+// // // // // // //"33abc"=> NaN
+// // // // // // // true => 1
+// // // // // // // false=> 0
 
-let isLoggedIn="arjun"
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// // // // // // let isLoggedIn="arjun"
+// // // // // // let booleanIsLoggedIn=Boolean(isLoggedIn)
+// // // // // // console.log(booleanIsLoggedIn)
 
-//1=>true
-//0=> false
-// ""=> false
-//"arjun"=> true
+// // // // // // //1=>true
+// // // // // // //0=> false
+// // // // // // // ""=> false
+// // // // // // //"arjun"=> true
 
-//string to number
-let someNumber=33
-let stringtoNumber=String(someNumber)
-console.log(stringtoNumber)
-console.log( typeof stringtoNumber)
+// // // // // // //string to number
+// // // // // // let someNumber=33
+// // // // // // let stringtoNumber=String(someNumber)
+// // // // // // console.log(stringtoNumber)
+// // // // // // console.log( typeof stringtoNumber)
+
+// // // // // // let value =3
+// // // // // // let negvalue=-value
+// // // // // // console.log(negvalue)
+
+// // // // // // console.log(2**3) //used when we find to power of any numbers 
+
+// // // // // //add string 
+// // // // // let str1="arjun singh"
+// // // // // let str2=" negi"
+// // // // // let str3 =str1+str2;
+// // // // // console.log(str3)
+
+
+// // // // // //some operation in striong 
+// // // // // console.log("1"+2)
+// // // // // console.log(1+"2")
+// // // // // console.log("1"+2+2)
+// // // // // console.log(1+"2"+2)
+// // // // // console.log(1+2+"2")
+// // // // // console.log(1+2+"4"+4+5+"8");
+
+// // // // // console.log(3+4*5/5) 
+
+// // // // console.log(true)
+// // // // console.log(+true)
+// // // // // console.log(true+) not allowed
+
+// // // gamecount=100
+// // // gamecount++
+// // // console.log(gamecount)
+// // // ++gamecount
+// // // console.log(gamecount)
+
+// // // bsic comprasion
+// // // <,>,==;
+// // // comprasion b/w diffrent-2 data types
+
+// // console.log("2">1)
+// // console.log("02">1)
+// // console.log(null>0)
+// // console.log(null==0)
+// // console.log(null>=0)
+// // console.log("undedefind")
+// // console.log(undefined>0)
+// // console.log(undefined==0)
+// // console.log(undefined>=0)
+
+// // strict check(=== check with data type)
+// console.log("2"==2) because of conversion
+// console.log("2"===2) because check data types 
+
