664bc155b0853cea55051535b2c01c151e26f400
ARJUN SINGH NEGI <78934428+ARJU7500@users.noreply.github.com>
2023-12-25 16:34:23 +0000

    some changed

--- basicjs/basic_02variable.js
+++ basicjs/basic_02variable.js
@@ -1,9 +1,9 @@
 const accountId=1000987
-var accountGmail= "ram@108"
-let accountName="arjun negi"
+var accountGmail= "ram@108"  //use for global variable 
+let accountName="arjun negi"  //use in scope like function or inside the { } 
 accountCity="jaipur"
 
-// accountId=1009789 not allowed 
+// accountId=1009789 not allowed because we can't changed the value of const variable 
 accountGmail="shiv@108"
 accountName="himanshu"
 accountCity="ranikhet"
--- basicjs/basic_datatype.js
+++ basicjs/basic_datatype.js
@@ -1,4 +1,4 @@
 let name="Arjun Negi"
 let age= 26
 let num=7668213002
-console.log([name, age, num])
+console.log([name, age, num])  //it is sortcut to print multiple  variable at a one time
