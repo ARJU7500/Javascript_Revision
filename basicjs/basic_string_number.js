4c993fef90e001c4af54f571d61e1aec336e95df
arjun negi <an096237@gmail.com>
2023-12-25 23:03:01 +0530

    changed conversion

--- basicjs/basic_string_number.js
+++ basicjs/basic_string_number.js
@@ -1,79 +1,39 @@
-// // // // // // let score= undefined
-// // // // // // console.log(typeof score)
-// // // // // // console.log(typeof(score))
+// video 6
 
-// // // // // // let valueInnumber=Number(score)
-// // // // // // console.log(typeof valueInnumber);
-// // // // // // console.log(valueInnumber)
+// "use strict"; 
+//it means we used newer version of javaScript but now it is automatic
 
-// // // // // // //"33" => 33
-// // // // // // //"33abc"=> NaN
-// // // // // // // true => 1
-// // // // // // // false=> 0
+// let score=33 
+// here o/p is number 
+// let score="33"
+// //o/p is string
+// console.log(typeof score)
+// console.log(typeof(score))
 
-// // // // // // let isLoggedIn="arjun"
-// // // // // // let booleanIsLoggedIn=Boolean(isLoggedIn)
-// // // // // // console.log(booleanIsLoggedIn)
+// let score="33abc" o/p is NaN(not an number)
+// let score = null //o/p is 0
+// let score =undefined; //=>o/p is NaN
+// let score =true; //=>o/p is 1
+// let score =false; =>o/p is 0
+// let score ="arjun" //=>o/p is NaN
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// valueInNumber type is number
+// console.log(valueInNumber);
+//output is NaN
 
-// // // // // // //1=>true
-// // // // // // //0=> false
-// // // // // // // ""=> false
-// // // // // // //"arjun"=> true
+// CONVER INTO BOOLEAN=>
 
-// // // // // // //string to number
-// // // // // // let someNumber=33
-// // // // // // let stringtoNumber=String(someNumber)
-// // // // // // console.log(stringtoNumber)
-// // // // // // console.log( typeof stringtoNumber)
+// let isBoolean=1 =>true
+// let isBoolean=0 =>false
+// let isBoolean="" =>false
+// let isBoolean="arjun" =>true
+// let booleanis=Boolean(isBoolean)
+// console.log(booleanis); 
 
-// // // // // // let value =3
-// // // // // // let negvalue=-value
-// // // // // // console.log(negvalue)
+// CONVERT INTO STRING =>
 
-// // // // // // console.log(2**3) //used when we find to power of any numbers 
-
-// // // // // //add string 
-// // // // // let str1="arjun singh"
-// // // // // let str2=" negi"
-// // // // // let str3 =str1+str2;
-// // // // // console.log(str3)
-
-
-// // // // // //some operation in striong 
-// // // // // console.log("1"+2)
-// // // // // console.log(1+"2")
-// // // // // console.log("1"+2+2)
-// // // // // console.log(1+"2"+2)
-// // // // // console.log(1+2+"2")
-// // // // // console.log(1+2+"4"+4+5+"8");
-
-// // // // // console.log(3+4*5/5) 
-
-// // // // console.log(true)
-// // // // console.log(+true)
-// // // // // console.log(true+) not allowed
-
-// // // gamecount=100
-// // // gamecount++
-// // // console.log(gamecount)
-// // // ++gamecount
-// // // console.log(gamecount)
-
-// // // bsic comprasion
-// // // <,>,==;
-// // // comprasion b/w diffrent-2 data types
-
-// // console.log("2">1)
-// // console.log("02">1)
-// // console.log(null>0)
-// // console.log(null==0)
-// // console.log(null>=0)
-// // console.log("undedefind")
-// // console.log(undefined>0)
-// // console.log(undefined==0)
-// // console.log(undefined>=0)
-
-// // strict check(=== check with data type)
-// console.log("2"==2) because of conversion
-// console.log("2"===2) because check data types 
-
+// let someNumber=33; =>convrt into string 
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
