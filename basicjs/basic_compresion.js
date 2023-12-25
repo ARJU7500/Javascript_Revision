f487481060b6d2ce3973cc054af5542788083754
arjun negi <an096237@gmail.com>
2023-12-25 23:49:58 +0530

    add data type summary

---
+++ basicjs/basic_compresion.js
@@ -0,0 +1,43 @@
+// == and ===
+// check the value same or not while === check value wih data types
+
+// *************************data types summary******************************
+
+// javascript is dynamic typed
+
+// there are two types of data types in javascript
+// 1. premptive or call by value
+//     string, number, null, undefined, symbol, bigint
+// 2. non-premptive or reference type
+//     Array, object, functions
+
+// decleare symbol=> data types is symbol
+// let id= Symbol('123')
+// console.log(id); =>Symbol(123)
+
+// about bigint 
+// let bigNumber=12345678901234678n
+// console.log(bigNumber);
+
+// ARRAY=>
+let heros=["abdul klam sir", "bhgt singh"];
+//=> data types is function
+//OBJECT
+let myObj={
+    name:"ambedkr",
+    age:89
+}
+
+// function=>
+
+let myfun=functiom ()  //=> data type object function  
+{
+// console.log("arjun negi");
+}
+
+//how to find data type 
+
+// we use typeof
+
+// console.log(typeof variable_name); or
+// console.log(typeof(variable_name));
