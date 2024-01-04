7ff647ea7c2f36633d3bcf7cd827ac24743298a4
arjun negi <an096237@gmail.com>
2024-01-04 20:44:29 +0530

    4jan2024

--- projectofjs/BMI Genrater/bmigen.js
+++ projectofjs/BMI Genrater/bmigen.js
@@ -6,6 +6,21 @@ form.addEventListener('submit', (e)=>{
     const results = document.querySelector('#result');
     if(height===''||height<0||isNaN(height))
     {
-        results.innerHTML = `<h1>please give a valid height:${height}</h1>`;
+        results.innerHTML = `<p>please give a valid height:${height}</p>`;
     }
+    else if(weight===''||weight<0||isNaN(weight))
+    {
+        results.innerHTML = `<p>please give a valid weight:${weight}</p>`;
+    }
+    else 
+    {
+    const bmi = (weight/((height*height)/10000)).toFixed(2)
+    if(bmi<18.60)
+        results.innerHTML = `<span>${bmi} is under weight </span>`
+    else if(bmi>18.60)
+        results.innerHTML = `<span>${bmi} is normal </span>`
+    else(bmi>24.90)
+        results.innerHTML = `<span>${bmi} is over weight </span>`
+    }
+
 })
